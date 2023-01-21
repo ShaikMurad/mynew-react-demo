1b280c27cc22d6e6d8ad0ee46f3d0ab37498f8e7
ShaikMurad <shaikmurad786@gmail.com>
2023-01-21 12:19:44 +0530

    Toggle Summary update

--- src/App.jsx
+++ src/App.jsx
@@ -136,21 +136,23 @@ function MovieList() {
   )
 }
 function Movie({ movie }) {
-  // const movie = {
-  //   "name": "Vikram",
-  //   "poster": "https://m.media-amazon.com/images/M/MV5BMmJhYTYxMGEtNjQ5NS00MWZiLWEwN2ItYjJmMWE2YTU1YWYxXkEyXkFqcGdeQXVyMTEzNzg0Mjkx._V1_.jpg",
-  //   "rating": 8.4,
-  //   "summary": "Members of a black ops team must track and eliminate a gang of masked murderers."
-  // };
-
+  const [Show, setShow] = useState(true);
+  const styles = {
+    color: movie.rating > 8.5 ? "green" : "crimson",
+  };
+  const summarystyles = {
+    display: Show ? "block" : "none",
+  };
   return (
     <div className="movie-container">
       <img className="movie-poster " src={movie.poster} alt={movie.name} />
       <div className="movie-spec">
         <h2 className="movie-name">{movie.name}</h2>
-        <p className="movie-rating">⭐{movie.rating}</p>
+        <p style={styles} className="movie-rating">⭐{movie.rating}</p>
       </div>
-      <p className="movie-summary">{movie.summary}</p>
+      <button onClick={() => setShow(!Show)}>Toggle Summary</button>
+      <p style={summarystyles} className="movie-summary">{movie.summary}</p>
+
       <Counter />
     </div>
   )
